46304fbf1a3a51d14ed03105bb39d6a32b844b4a
Jay Logelin <jay.logelin@gmail.com>
2024-10-11 14:49:01 -0300

    refactor(compute): Move tests: update MUX gate implementation and circuit execution efficiency

--- compute/src/uint.rs
+++ compute/src/uint.rs
@@ -14,7 +14,7 @@ pub type GarbledUint128 = GarbledUint<128>;
 // Define a new type Uint<N>
 #[derive(Debug, Clone)]
 pub struct GarbledUint<const N: usize> {
-    pub(crate) bits: Vec<bool>,       // Store the bits of the unsigned integer
+    pub bits: Vec<bool>,              // Store the bits of the unsigned integer
     _phantom: PhantomData<[bool; N]>, // PhantomData to ensure the N bit size
 }
 
@@ -212,59 +212,4 @@ impl<const N: usize> From<GarbledUint<N>> for u128 {
 
 // test conversions
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_display() {
-        let a: GarbledUint8 = 170u8.into(); // Binary 10101010
-        assert_eq!(format!("{}", a), "170");
-
-        let b: GarbledUint16 = 43707u16.into(); // Binary 1010101010101011
-        assert_eq!(format!("{}", b), "43707");
-
-        let c: GarbledUint32 = 2863311530u32.into(); // Binary 10101010101010101010101010101010
-        assert_eq!(format!("{}", c), "2863311530");
-
-        let d: GarbledUint64 = 12297829382473034410u64.into(); // Binary 1010101010101010101010101010101010101010101010101010101010101010
-        assert_eq!(format!("{}", d), "12297829382473034410");
-
-        let e: GarbledUint128 = 12297829382473034410u128.into(); // Binary 1010101010101010101010101010101010101010101010101010101010101010
-        assert_eq!(format!("{}", e), "12297829382473034410");
-    }
-
-    #[test]
-    fn test_from_u8() {
-        let a: GarbledUint8 = 170u8.into(); // Binary 10101010
-        let value: u8 = a.into();
-        assert_eq!(value, 170);
-    }
-
-    #[test]
-    fn test_from_u16() {
-        let a: GarbledUint16 = 43707u16.into(); // Binary 1010101010101011
-        let value: u16 = a.into();
-        assert_eq!(value, 43707);
-    }
-
-    #[test]
-    fn test_from_u32() {
-        let a: GarbledUint32 = 2863311530u32.into(); // Binary 10101010101010101010101010101010
-        let value: u32 = a.into();
-        assert_eq!(value, 2863311530);
-    }
-
-    #[test]
-    fn test_from_u64() {
-        let a: GarbledUint64 = 12297829382473034410u64.into(); // Binary 1010101010101010101010101010101010101010101010101010101010101010
-        let value: u64 = a.into();
-        assert_eq!(value, 12297829382473034410);
-    }
-
-    #[test]
-    fn test_from_u128() {
-        let a: GarbledUint128 = 12297829382473034410u128.into(); // Binary 1010101010101010101010101010101010101010101010101010101010101010
-        let value: u128 = a.into();
-        assert_eq!(value, 12297829382473034410);
-    }
-}
+mod tests {}
