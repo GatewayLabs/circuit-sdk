0a01947a8bd56eecb80fc85e285d50d292108379
Jay Logelin <jay.logelin@gmail.com>
2024-10-12 23:02:50 -0300

    feat(compute): Implement AddAssign, SubAssign, and MulAssign for GarbledUint and GarbledInt
    This commit refactors the arithmetic operations in the `compute` module to implement the `AddAssign`, `SubAssign`, and `MulAssign` traits for the `GarbledUint` and `GarbledInt` types. These traits allow for in-place addition, subtraction, and multiplication operations, improving the efficiency of circuit execution.

--- compute/src/operations/arithmetic.rs
+++ compute/src/operations/arithmetic.rs
@@ -3,7 +3,7 @@ use crate::operations::circuits::{
     build_and_execute_addition, build_and_execute_multiplication, build_and_execute_subtraction,
 };
 use crate::uint::GarbledUint;
-use std::ops::{Add, Mul, Sub};
+use std::ops::{Add, AddAssign, Mul, MulAssign, Sub, SubAssign};
 
 // Implement the Add operation for Uint<N> and &GarbledUint<N>
 impl<const N: usize> Add for GarbledUint<N> {
@@ -19,6 +19,19 @@ impl<const N: usize> Add for &GarbledUint<N> {
 
     fn add(self, rhs: Self) -> Self::Output {
         build_and_execute_addition(self, rhs)
+    }
+}
+
+// Implement the AddAssign operation for Uint<N> and &GarbledUint<N>
+impl<const N: usize> AddAssign for GarbledUint<N> {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = build_and_execute_addition(&self, &rhs);
+    }
+}
+
+impl<const N: usize> AddAssign<&GarbledUint<N>> for GarbledUint<N> {
+    fn add_assign(&mut self, rhs: &Self) {
+        *self = build_and_execute_addition(&self, rhs);
     }
 }
 
@@ -39,6 +52,19 @@ impl<const N: usize> Sub for &GarbledUint<N> {
     }
 }
 
+// Implement the SubAssign operation for GarbledUint<N> and &GarbledUint<N>
+impl<const N: usize> SubAssign for GarbledUint<N> {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = build_and_execute_subtraction(&self, &rhs);
+    }
+}
+
+impl<const N: usize> SubAssign<&GarbledUint<N>> for GarbledUint<N> {
+    fn sub_assign(&mut self, rhs: &Self) {
+        *self = build_and_execute_subtraction(&self, rhs);
+    }
+}
+
 impl<const N: usize> Mul for GarbledUint<N> {
     type Output = GarbledUint<N>;
 
@@ -52,6 +78,19 @@ impl<const N: usize> Mul for &GarbledUint<N> {
 
     fn mul(self, rhs: Self) -> Self::Output {
         build_and_execute_multiplication(self, rhs)
+    }
+}
+
+// Implement the MulAssign operation for GarbledUint<N> and &GarbledUint<N>
+impl<const N: usize> MulAssign for GarbledUint<N> {
+    fn mul_assign(&mut self, rhs: Self) {
+        *self = build_and_execute_multiplication(&self, &rhs);
+    }
+}
+
+impl<const N: usize> MulAssign<&GarbledUint<N>> for GarbledUint<N> {
+    fn mul_assign(&mut self, rhs: &Self) {
+        *self = build_and_execute_multiplication(&self, rhs);
     }
 }
 
@@ -72,6 +111,19 @@ impl<const N: usize> Add for &GarbledInt<N> {
     }
 }
 
+// Implement the AddAssign operation for GarbledInt<N> and &GarbledInt<N>
+impl<const N: usize> AddAssign for GarbledInt<N> {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = build_and_execute_addition(&self.clone().into(), &rhs.into()).into();
+    }
+}
+
+impl<const N: usize> AddAssign<&GarbledInt<N>> for GarbledInt<N> {
+    fn add_assign(&mut self, rhs: &Self) {
+        *self = build_and_execute_addition(&self.clone().into(), &rhs.into()).into();
+    }
+}
+
 // Implement the Sub operation for GarbledInt<N> and &GarbledInt<N>
 impl<const N: usize> Sub for GarbledInt<N> {
     type Output = Self;
@@ -86,5 +138,48 @@ impl<const N: usize> Sub for &GarbledInt<N> {
 
     fn sub(self, rhs: Self) -> Self::Output {
         build_and_execute_subtraction(&self.into(), &rhs.into()).into()
+    }
+}
+
+// Implement the SubAssign operation for GarbledInt<N> and &GarbledInt<N>
+impl<const N: usize> SubAssign for GarbledInt<N> {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = build_and_execute_subtraction(&self.clone().into(), &rhs.into()).into();
+    }
+}
+
+impl<const N: usize> SubAssign<&GarbledInt<N>> for GarbledInt<N> {
+    fn sub_assign(&mut self, rhs: &Self) {
+        *self = build_and_execute_subtraction(&self.clone().into(), &rhs.into()).into();
+    }
+}
+
+// Implement the Mul operation for GarbledInt<N> and &GarbledInt<N>
+impl<const N: usize> Mul for GarbledInt<N> {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        build_and_execute_multiplication(&self.into(), &rhs.into()).into()
+    }
+}
+
+impl<const N: usize> Mul for &GarbledInt<N> {
+    type Output = GarbledInt<N>;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        build_and_execute_multiplication(&self.into(), &rhs.into()).into()
+    }
+}
+
+// Implement the MulAssign operation for GarbledInt<N> and &GarbledInt<N>
+impl<const N: usize> MulAssign for GarbledInt<N> {
+    fn mul_assign(&mut self, rhs: Self) {
+        *self = build_and_execute_multiplication(&self.clone().into(), &rhs.into()).into();
+    }
+}
+
+impl<const N: usize> MulAssign<&GarbledInt<N>> for GarbledInt<N> {
+    fn mul_assign(&mut self, rhs: &Self) {
+        *self = build_and_execute_multiplication(&self.clone().into(), &rhs.into()).into();
     }
 }
